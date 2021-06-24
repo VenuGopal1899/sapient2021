3d0f931f9a332669e1e1aafb61e05d4a79534de8
prabhat1510 <prabhatc@gmail.com>
2021-06-24 08:16:25 +0530

    JPA working examples
    Basic JPA connections with CRUD and Query

--- Day13/jpatraining/src/main/java/jpatraining/app/MemberApp.java
+++ Day13/jpatraining/src/main/java/jpatraining/app/MemberApp.java
@@ -3,8 +3,10 @@
  */
 package jpatraining.app;
 
-import jpatraining.dao.MemberDAO;
+import java.util.List;
+
 import jpatraining.dao.MemberDAOImpl;
+import jpatraining.entities.Member;
 
 /**
  * @author admi
@@ -21,7 +23,27 @@ public class MemberApp {
 		MemberDAOImpl memberDao =  new MemberDAOImpl();
 		boolean isMemberAvailable;
 		isMemberAvailable=memberDao.isMemberAvailable(11111);
+		System.out.println(isMemberAvailable);
+		System.out.println("*************************************");
+		//Add member
+		Member member = new Member();
+		member.setId(7);
+		member.setName("Cisco");
+		String memberAdded = memberDao.addMember(member);
+		System.out.println(memberAdded);
+		System.out.println("*************************************");
+		//Get all members
+		List<Member> membersList =memberDao.findAllMembers();
+		System.out.println(membersList);
 
+		//Update member
+		Member updatedMember = new Member();
+		updatedMember.setId(2);
+		updatedMember.setName("Oracle");
+		memberDao.updateMember(updatedMember);
+		System.out.println("*************************************");
+		List<Member> updatedMembersList =memberDao.findAllMembers();
+		System.out.println(updatedMembersList);
 	}
 
 }
